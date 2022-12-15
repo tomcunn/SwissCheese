2b69f824925e5240553deac21a2a275c0d62dbee
Tom <tomacunningham@gmail.com>
2022-12-14 22:08:12 -0600

    Project working now.

--- DualStepperControl/DualStepperControl.ino
+++ DualStepperControl/DualStepperControl.ino
@@ -10,7 +10,7 @@
 #include <avr/interrupt.h>
 
 //Hardware Assignments
-#define  MAX_VELOCITY  30000
+#define  MAX_VELOCITY  90000
 #define  MAX_ACCELERATION  2000
 
 #define StepA    A0
@@ -27,7 +27,8 @@ void  SetupTimer1(void);
 void  SetupTimer0(void);
 void  MotionAccelerationControl(struct MovementParms *MotorCurrent, struct MovementParms *MotorDesired);
 void  MotionVelocityControl    (struct MovementParms *MotorCurrent, struct MovementParms *MotorDesired);
-unsigned int WriteStepSize(struct MovementParms *Motor);
+void  WriteStepSize(struct MovementParms *Motor);
+static int counter=0;
 
 //Create a structure for movement data
 struct MovementParms
@@ -35,6 +36,7 @@ struct MovementParms
   signed int Position;
   signed int Velocity;
   signed int Acceleration;
+  unsigned int TimerMatchValue;
   bool Direction;
 };
 
@@ -47,21 +49,23 @@ volatile struct MovementParms MotorB_Current;
 
 //Static Variables that are accessed in interrupts
 volatile unsigned int abs_Current_Velocity = 0;
-volatile unsigned int Counter = 0;
-volatile unsigned int StepSpeedA = 0;
-volatile unsigned int StepSpeedB = 0;
-volatile signed long timestep = 0;
-volatile bool RunTask_10ms = 0;
+static volatile unsigned int Counter = 0;
+static volatile unsigned int StepSpeedA = 0;
+static volatile unsigned int StepSpeedB = 0;
+static volatile unsigned int tempA = 0;
+static volatile unsigned int tempB = 0;
+static volatile signed long timestep = 0;
+static volatile bool RunTask_10ms = 0;
+static volatile bool flopa = 0;
+static volatile bool flopb = 0;
 
 //**************************************
 // Timer1 - 16 bit timer Interrupt  
 // Motor A
 // Setup for ATMEGA2560
 //**************************************
-ISR(TIMER1_COMPA_vect)  
+ISR(TIMER4_COMPA_vect)  
 {   
-   if(StepSpeedA < 64000)
-   {
      //Check the direction pin and determine the position
      if(MotorA_Current.Direction == 1)
      {
@@ -73,16 +77,21 @@ ISR(TIMER1_COMPA_vect)
        digitalWrite(DirA,0);
        MotorA_Current.Position--;
      }
+    
+     if(flopa)
+     {
+        digitalWrite(StepA,1);
+        flopa = 0;
+     }
+    else
+    {
+        digitalWrite(StepA,0);
+        flopa=1;
+    }
+
+   //tempA =StepSpeedA;
+   OCR4A= StepSpeedA;
   
-     //Step once
-     digitalWrite(StepA,1);
-     digitalWrite(StepA,0);
-   }
-   //Interrupt occurs when timer matches this value
-   //Larger velocity means smaller OCR1A value
-   //StepSpeed is calculated using another function and then stored 
-   //in a global variable to be accessed by the interrupt routine
-   OCR1A = StepSpeedA;
 }
 
 //**************************************
@@ -92,8 +101,6 @@ ISR(TIMER1_COMPA_vect)
 //**************************************
 ISR(TIMER3_COMPA_vect)  
 {   
-   if(StepSpeedB < 64000)
-   {
      //Check the direction pin and determine the position
      if(MotorB_Current.Direction == 1)
      {
@@ -105,16 +112,21 @@ ISR(TIMER3_COMPA_vect)
        digitalWrite(DirB,0);
        MotorB_Current.Position--;
      }
-  
-     //Step once
-     digitalWrite(StepB,1);
-     digitalWrite(StepB,0);
-   }
-   //Interrupt occurs when timer matches this value
-   //Larger velocity means smaller OCR1A value
-   //StepSpeed is calculated using another function and then stored 
-   //in a global variable to be accessed by the interrupt routine
-   OCR3A = StepSpeedB;
+
+
+     if(flopb)
+     {
+        digitalWrite(StepB,1);
+        flopb = 0;
+     }
+    else
+    {
+        digitalWrite(StepB,0);
+        flopb=1;
+    }
+   
+   //tempB = StepSpeedB;
+   OCR3A =  StepSpeedB;
 }
 
 //**************************************
@@ -141,7 +153,7 @@ ISR(TIMER0_COMPA_vect)
 // to generate the stepper pulses. Speed is in 
 // the units mm/min. 
 //*******************************************
-unsigned int WriteStepSize(struct MovementParms *Motor)
+void WriteStepSize(struct MovementParms *Motor)
 {
   unsigned int StepSpeed = 0;
   //Set the direction pin based on the sign of the speed
@@ -153,64 +165,48 @@ unsigned int WriteStepSize(struct MovementParms *Motor)
   {
      Motor->Direction = 0;
   } 
-  // Conversion Factor
-  //  20000 mm      1 min      360 deg     1 rev       step          4,444 step
-  // ----------- x -------- x -------- x -------- x ------------ =  ---------
-  //     min        60 sec      rev        120 mm     0.225 deg       sec
-  //
-  //   16,000,000 cycles      1 sec            3,600 cycles
-  //   ----------------- x --------------- = -----------
-  //       1 sec             4,444 step          
-  //  16,000,000/4,444    = 3,600 clock cycles  (20000 mm/min)
-  //  16,000,000/2,222    = 7,200 clock cycles  (10000 mm/min)
-  //  16,000,000/666      = 24,024 clock cycles  (3000 mm/min)
-  
-  //For some reason the prescale set to zero causes problems, so 8 is the minimum
-  //which means the clock is 16,000,000/8=2,000,000
-  //  2,000,000/4,444   = 450 clock cycles  (20000 mm/min)
-  //  2,000,000/2,222    = 900 clock cycles  (10000 mm/min)
-  //  2,000,000/666      = 3003 clock cycles (3000 mm/min)
+  // Conversion Factor in One Note
+  // 1:1 Prescale = 16,000,000Hz
+  // 200*16microsteps = 3200 steps/rev
+  // 27.03 steps/mm
 
-  //To compute position it is 
-  // Conversion Factor
-  //    360 deg     1 rev       step          13.33 steps
-  //  -------- x -------- x ------------ =  ---------
-  //     rev        120 mm     0.225 deg        mm
-
-  
-  
   //Check to prevent overflows, StepSpeed is 16 bit
-  if(Motor->Velocity > 700 || Motor->Velocity < -700)
+  // 700 mm/sec is VERY slow, 0.25 rev/sec.
+  if(((Motor->Velocity) > 700) || ((Motor->Velocity) < -700))
   {
      //This is derived from the table above using a Power trendline
-     StepSpeed = (signed int)(9048739L/(abs(Motor->Velocity)));
+      Motor->TimerMatchValue = (unsigned int)(16000000L/(abs(Motor->Velocity)));
   }
   //This is like setting it to zero speed
   else
   {
-     StepSpeed = 65000;
+   Motor->TimerMatchValue = 65000;
+     
   }
-  return StepSpeed;
 }
 //*******************************************
 //  Configure Timer 1 - 16 bit timer
 //  Atmega2560
 //  Timer/Counter 1, 3, 4, and 5
 //*******************************************
-void SetupTimer1()
+void SetupTimer4()
 {
+  //Clear the register
+  TCCR4A = 0;
+  TCCR4B = 0;
 
   //Set the prescaler
-  TCCR1B|=(1<<CS10);// Clock/1
+  TCCR4B|=(1<<CS10);// Clock/1
 
   //Start counting at zero and then interrupt on match
-  TCCR1B|=(1<<WGM12); // Set in CTC mode
+  TCCR4B|=(1<<WGM12); // Set in CTC mode
 
   //The value of the match.
-  OCR1A=250; // Set Output compare to 250;
+  OCR4A=0; // Set Output compare to 250;
 
-  //Enable to interrupt on timer 1
-  TIMSK1 |= (1<<OCIE1A); 
+  TIFR4 = 0xFF;
+
+
 }
 
 //*********************************************
@@ -218,6 +214,10 @@ void SetupTimer1()
 //*********************************************
 void SetupTimer3()
 {
+  //Clear the register
+  TCCR3A = 0;
+  TCCR3B = 0;
+
   //Set the prescaler
   TCCR3B|=(1<<CS10);// Clock/1
 
@@ -225,10 +225,10 @@ void SetupTimer3()
   TCCR3B|=(1<<WGM12); // Set in CTC mode
 
   //The value of the match.
-  OCR3A=250; // Set Output compare to 250;
+  OCR3A=0; // Set Output compare to 250;
+ 
+  TIFR3 = 0xFF;
 
-  //Enable to interrupt on timer 1
-  TIMSK3 |= (1<<OCIE3A); 
 }
 
 //*********************************************
@@ -259,6 +259,9 @@ void SetupTimer0()
 //*****************************************
 void setup() 
 {
+  //Disable interrupts
+  cli();
+  
   //Set the pin modes
   pinMode(StepA,OUTPUT);
   pinMode(DirA,OUTPUT);
@@ -266,17 +269,41 @@ void setup()
   pinMode(StepB,OUTPUT);
   pinMode(DirB,OUTPUT);
 
-  delay(50);
+  pinMode(EnableA,OUTPUT);
+  pinMode(EnableB,OUTPUT);
 
-  //Disable interrupts
-  cli();
   //Setup Timers
   SetupTimer3();
-  SetupTimer1();
+  SetupTimer4();
   SetupTimer0();
+  delay(1000);
+  
+  Serial.begin(115200);
+
+  Serial.println("This is the setup TCCR1A");
+  Serial.println(TCCR1A);
+
+  Serial.println("This is the setup TCCR3A");
+  Serial.println(TCCR3A);
+
+  Serial.println("This is the setup TCCR1B");
+  Serial.println(TCCR1B);
+
+  Serial.println("This is the setup TCCR3B");
+  Serial.println(TCCR3B);
+
+  digitalWrite(DirB,1);
+  digitalWrite(DirA,1);
+  digitalWrite(EnableA, LOW);
+  digitalWrite(EnableB, LOW);
+
+  MotorA_Current.Position = 0;
+  MotorB_Current.Position = 0;
+
+  delay(2000);
+
   //enable interrupts
   sei();
-  Serial.begin(115200);
   
 }
 
@@ -286,11 +313,16 @@ void setup()
 //********************************************
 void loop() 
 {
+  static bool FirstLoop = true;
 
   if(RunTask_10ms)
   {
-    MotorA_Desired.Acceleration = 2000;
-    MotorB_Desired.Acceleration = 2000;
+
+    MotorA_Desired.Acceleration = 50;
+    MotorB_Desired.Acceleration = 50;
+
+    MotorA_Desired.Velocity = 25000;
+    MotorB_Desired.Velocity = 25000;
     
     //Call this function to compute the current velocity,this
     //takes into account the acceleration
@@ -298,10 +330,25 @@ void loop()
     MotionVelocityControl(&MotorB_Current, &MotorB_Desired);
 
     //Call function to covert the velocity from mm/min to timer value
-    StepSpeedA = WriteStepSize(&MotorA_Current);
-    StepSpeedB = WriteStepSize(&MotorB_Current);
+    WriteStepSize(&MotorA_Current);
+    WriteStepSize(&MotorB_Current);
 
-    
+    StepSpeedA = MotorA_Current.TimerMatchValue;
+    StepSpeedB = MotorB_Current.TimerMatchValue;
+
+    Serial.print(MotorA_Current.Position);
+    Serial.print(",");
+    Serial.println(MotorB_Current.Position);
+
+    if(FirstLoop)
+    {
+      //Enable to interrupt on timer 1
+      TIMSK3 |= (1<<OCIE3A); 
+      ////Enable to interrupt on timer 1
+      TIMSK4 |= (1<<OCIE4A);
+      // 
+      FirstLoop = false;
+    }
     //Clear the flag that gets set by the interrupt
     //This was added at end to detect overruns of the 10ms task
     RunTask_10ms = 0;
